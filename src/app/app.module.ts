b348c667c79c9bb0b9b8c7e02bab3b177f47b7a5
Joel Mondesir <joey.M.Dev@gmail.com>
2020-07-14 17:31:22 -0500

    carousel edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PromoComponent } from './components/promo/promo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SubmenuComponent } from './components/submenu/submenu.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainDisplayComponent } from './components/main-display/main-display.component';
+import { MainCarouselComponent } from './components/carousel/main-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromoComponent,
+    SubmenuComponent,
+    NavigationComponent,
+    ToolbarComponent,
+    MainDisplayComponent,
+    MainCarouselComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+
+  imports: [BrowserModule, BrowserAnimationsModule, NgbModule],
+
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
