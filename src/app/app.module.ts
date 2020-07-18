89effef5cfa6f533e2690a0de328161d12dbe502
Joel Mondesir <joey.M.Dev@gmail.com>
2020-07-17 23:15:50 -0500

    carousel teardown and rebuild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,15 @@ import { AppComponent } from './app.component';
 import { PromoComponent } from './components/promo/promo.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { SubmenuComponent } from './components/submenu/submenu.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MainDisplayComponent } from './components/main-display/main-display.component';
-import { MainCarouselComponent } from './components/carousel/main-carousel.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PromoComponent,
-    SubmenuComponent,
     NavigationComponent,
-    ToolbarComponent,
-    MainDisplayComponent,
-    MainCarouselComponent,
+    CarouselComponent,
   ],
 
   imports: [BrowserModule, BrowserAnimationsModule, NgbModule],
--- src/app/components/carousel/carousel.component.spec.ts
+++ src/app/components/carousel/carousel.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainCarouselComponent } from './main-carousel.component';
+import { CarouselComponent } from './carousel.component';
 
-describe('MainCarouselComponent', () => {
-  let component: MainCarouselComponent;
-  let fixture: ComponentFixture<MainCarouselComponent>;
+describe('CarouselComponent', () => {
+  let component: CarouselComponent;
+  let fixture: ComponentFixture<CarouselComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainCarouselComponent ]
+      declarations: [ CarouselComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MainCarouselComponent);
+    fixture = TestBed.createComponent(CarouselComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
